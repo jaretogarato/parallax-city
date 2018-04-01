faebae90d48052324a68a55ba38ff099552c670e
Jared Gallardo <jaredgg@gmail.com>
2018-04-01 16:11:32 -0600

    correct float / int layer positioning edge case bugs

--- js/parallax-city.js
+++ js/parallax-city.js
@@ -3,39 +3,41 @@
 /*jslint plusplus: true */
 /*global $, jQuery, TimelineLite, TimelineMax, TweenMax, TweenLite, Power2, console*/
 
-// TODO: make steer speed continuous
-// TODO: cause braking when bg or cloud edges reach viewport edges
-// TODO: add speed transitions
+// TODO: recalculate positions when window is resized
 
 // get mouse position, send velocity
 var mouseIsHovering = false, xMousePos, yMousePos;
 var bgXisFullyVisible = false, bgYisFullyVisible = false;
 
 $('#wrapper').mouseenter(function(){
-    mouseIsHovering = true;
+  mouseIsHovering = true;
 }).mouseleave(function(){
-    mouseIsHovering = false;
+  mouseIsHovering = false;
 });
 
-$("#wrapper").mousemove(function (event) {
-    xMousePos = event.pageX;
-    yMousePos = event.pageY;
+$("#wrapper").mousemove(function(event) {
+  xMousePos = event.pageX;
+  yMousePos = event.pageY;
 });
 
 // constructor function: layers
 function MovingLayer(layerId, left, top, width, depth) {
+  var xtrans, ytrans;
+
   this.layerId = layerId;
   this.top = top;
   this.left = left;
   if (width === 0){
     this.width = $(this.layerId).css('width');
   } else {
-    this.width = width;
+    this.width = width+"px";
   }
   this.height = $(this.layerId).css('height');
   this.depth = depth;
 
   // console.log('layerId: ', this.layerId);
+  // console.log('this.top: ', this.top);
+  // console.log('this.left: ', this.left);
   // console.log('layer width: ', this.width);
   // console.log('layer height: ', this.height);
   // ^^^ ok
@@ -46,27 +48,37 @@ function MovingLayer(layerId, left, top, width, depth) {
 
   this.layerMove = function () {
     var
-      xtrans, ytrans, xtransn, ytransn,
+      xtransn, ytransn,
       steerXspeed, steerXdir, steerXpositive = 1,
       steerYspeed, steerYdir, steerYpositive = 1,
+      targXspeed, targYspeed,
       viewCentX, viewCentY,
       boundingRect, layerTop, layerBottom, layerLeft, layerRight,
+      windowWidth = $(window).width(),
+      windowHeight = $(window).height(),
       layerIdShort;
-      windowWidth = $(window).width();
-      windowHeight = $(window).height();
 
-    xtrans = parseFloat($(this.layerId).css('transform').split(',')[4], 10);
-    ytrans = parseFloat($(this.layerId).css('transform').split(',')[5], 10);
+    // xtrans = parseFloat($(this.layerId).css('transform').split(',')[4], 10);
+    // ytrans = parseFloat($(this.layerId).css('transform').split(',')[5], 10);
+
+    if(isNaN(xtrans)) {
+      xtrans = 0;
+    }
+    if(isNaN(ytrans)) {
+      ytrans = 0;
+    }
+
     viewCentX = windowWidth / 2;
     viewCentY = windowHeight / 2;
     steerXdir = viewCentX - xMousePos;
     steerYdir = viewCentY - yMousePos;
 
-    // if (this.layerId == '#layer000'){
     layerIdShort = this.layerId;
     if(layerIdShort.charAt(0) === '#'){
-     layerIdShort = layerIdShort.substr(1);
+      layerIdShort = layerIdShort.substr(1);
     }
+
+    // find the edges of each layer
     boundingRect = document.getElementById(layerIdShort).getBoundingClientRect();
     layerTop = boundingRect.top;
     layerBottom = layerTop + $(this.layerId).height();
@@ -74,9 +86,6 @@ function MovingLayer(layerId, left, top, width, depth) {
     layerLeft = boundingRect.left;
     layerRight = layerLeft + $(this.layerId).width();
     // layerRight = layerLeft + this.width;
-    // console.clear();
-    // console.log('width: ', $(this.layerId).width());
-    // console.log('height: ', $(this.layerId).height());
 
     // console.log('layer: ', this.layerId);
     // console.log('left: ', layerLeft);
@@ -84,38 +93,20 @@ function MovingLayer(layerId, left, top, width, depth) {
     // console.log('top: ', layerTop);
     // console.log('bottom: ', layerBottom);
     // console.log('--------------');
-    // }
 
     // console.log("steerXdir: " + steerXdir + " steerYdir: " + steerYdir);
     // console.log($(layer000).css(width));
 
     if(mouseIsHovering){
+      // set l/r, u/d direction
       steerXdir > 0 ? steerXpositive = 1 : steerXpositive = -1;
       steerYdir > 0 ? steerYpositive = 1 : steerYpositive = -1;
 
-      // switch (true) {
-      //   case (Math.abs(steerXdir) < 100):
-      //     steerXspeed = 0;
-      //     break;
-      //   case (Math.abs(steerXdir) < 250):
-      //     steerXspeed = 0.5 * steerXpositive;
-      //     break;
-      //   case (Math.abs(steerXdir) >= 250):
-      //     steerXspeed = 1 * steerXpositive;
-      //     break;
-      // }
-
-      // if (Math.abs(steerXdir) < 100) {
-      //   steerXspeed = 0;
-      // } else {
-      //   steerXspeed = (Math.abs(steerXdir) / (viewCentX)) * steerXpositive * 2;
-      //   // console.log('steerXspeed:  ', steerXspeed);
-      // }
-
       // set speed based on mouse position
       // power of 2.7 builds speed quickly
-      steerXspeed = Math.pow((Math.abs(steerXdir) / (viewCentX)) * 2, 2.7) * steerXpositive;
-      steerYspeed = Math.pow((Math.abs(steerYdir) / (viewCentY)) * 2, 2.7) * steerYpositive;
+      steerXspeed = Math.pow((Math.abs(steerXdir) / (viewCentX)) * 2, 2.5) * steerXpositive;
+      steerYspeed = Math.pow((Math.abs(steerYdir) / (viewCentY)) * 2, 2.5) * steerYpositive;
+
       // stop movement when increment is small
       // choke top speed for mouse positions closer to viewport center
       if (Math.abs(steerXspeed) < 0.05) {
@@ -132,30 +123,51 @@ function MovingLayer(layerId, left, top, width, depth) {
       steerXdir = 0;
       steerYdir = 0;
     }
-    xtransn = Math.round(xtrans + (steerXspeed * this.depth));
-    ytransn = Math.round(ytrans + (steerYspeed * this.depth));
+    xtrans = xtrans + (steerXspeed * this.depth);
+    ytrans = ytrans + (steerYspeed * this.depth);
+    xtransn = Math.round(xtrans);
+    ytransn = Math.round(ytrans);
+
+    // console.log('xtrans: ', xtrans);
+    // console.log('ytrans: ', ytrans);
+    // console.log('xtransn: ', xtransn);
+    // console.log('ytrans: ', ytransn);
+    // console.log('-----');
+    // xtransn = Math.round(xtrans + (steerXspeed * this.depth));
+    // ytransn = Math.round(ytrans + (steerYspeed * this.depth));
     // console.log(xtrans + " *** " + ytrans);
 
+    // position the layers
     $(layerId).css({
       "-ms-transform": "translate(" + xtransn + "px, " + ytransn + "px)",
       "webkit-transform": "translate(" + xtransn + "px, " + ytransn + "px)",
       "transform": "translate(" + xtransn + "px, " + ytransn + "px)"
     });
     // console.log("xMousePos: " + xMousePos + " yMousePos: " + yMousePos);
-    // console.log("layerMove ran");
   };
 }
 
-// prototypes: layers (use here to prevent function creation for each instantiation)
+// prototypes: moving layers
+// (use here to prevent function creation for each instantiation)
 MovingLayer.prototype.layerSetup = function () {
   "use strict";
   $(this.layerId).css({
     "width": this.width,
     "height": this.height,
-    "left": this.left + "px",
-    "top": this.top + "px"
+    "left": this.left,
+    "top": this.top
   });
-  // console.log("layerSetup ran");
+  // console.log('layerId: ', this.layerId);
+  // console.log('this.top: ', this.top);
+  // console.log('this.left: ', this.left);
+  // console.log('layer width: ', this.width);
+  // console.log('layer height: ', this.height);
+  // console.log('-----');
+  // console.log('read top: ', $(this.layerId).position().top);
+  // console.log('read left: ', $(this.layerId).position().left);
+  // console.log('read width: ', $(this.layerId).width());
+  // console.log('read height: ', $(this.layerId).height());
+  // console.log('-------------------');
 };
 
 // images
@@ -169,14 +181,16 @@ MovingLayer.prototype.layerSetup = function () {
 
 // instantiate layers (and set properties, like position)
 // depth value is for parallax; higher values are closer to viewer
-// layername, transX, transY, width ('0' to use css) depth
+// arguments: layername, transX, transY, width ('0' to use css) depth
 
+// TODO: instead of fix %s for top and left below, calculate % based on
+//   size of viewport and size of image
 var
-  l000 = new MovingLayer('#layer000', -50, -50, 0, 1),
-  l001 = new MovingLayer('#layer001', 200, 200, 1789, 2),
-  l010 = new MovingLayer('#layer010', 300, 300, 2068, 7),
-  l050 = new MovingLayer('#layer050', -400, -400, 6437, 8),
-  l100 = new MovingLayer('#layer100', 400, 400, 410, 10);
+  l000 = new MovingLayer('#layer000', '-25%', '-25%', 0, 1.5),
+  l001 = new MovingLayer('#layer001', '-10%', '-10%', 1789, 2.5),
+  l010 = new MovingLayer('#layer010', '-15%', '-15%', 2068, 7),
+  l050 = new MovingLayer('#layer050', '-100%', '-150%', 6437, 5),
+  l100 = new MovingLayer('#layer100', '-20%', '-20%', 410, 10);
 
 l000.layerSetup();
 l001.layerSetup();
@@ -196,4 +210,4 @@ setInterval(function () {
   l050.layerMove();
   l100.layerMove();
   ++t;
-}, 1000 / 30);
+}, 1000 / 60);
