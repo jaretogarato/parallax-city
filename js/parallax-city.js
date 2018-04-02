b79010a1ab7299f31325a209492ec19716c71b15
Jared Gallardo <jaredgg@gmail.com>
2018-04-02 13:48:29 -0600

    add greensock, jquery, sun animation; reorder layers

--- js/parallax-city.js
+++ js/parallax-city.js
@@ -13,7 +13,7 @@ var
   moveLeftIsOk = 1, moveRightIsOk = 1,
   moveUpIsOk = 1, moveDownIsOk = 1,
   l000 = new MovingLayer('#layer000', '-25%', '-25%', 0, 0.35),
-  l001 = new MovingLayer('#layer001', '-10%', '-10%', 1789, 2.5),
+  l007 = new MovingLayer('#layer007', '-10%', '-10%', 1789, 2.5),
   l010 = new MovingLayer('#layer010', '-15%', '-15%', 2068, 5),
   l050 = new MovingLayer('#layer050', '-100%', '-150%', 6437, 6),
   l100 = new MovingLayer('#layer100', '-20%', '-20%', 410, 9);
@@ -29,6 +29,15 @@ $('#wrapper').mouseenter(function(){
 $("#wrapper").mousemove(function(event) {
   xMousePos = event.pageX;
   yMousePos = event.pageY;
+});
+
+$(window).on('load', function () { // load images before animating
+	"use strict";
+  TweenLite.delayedCall(0.1, function () { // start delay
+    $('.animatedSun').animateimage(10, -1).parent(); // activate animation on the animatedSun elements
+	});
+	// doLayout('ready');
+	// fly();
 });
 
 
@@ -203,7 +212,8 @@ MovingLayer.prototype.layerSetup = function () {
 
 // images
 // layer000 | jgg-photo  | main bg        | 2879x2400
-// layer001 | bldgs-03   | light blue     | 1789x1174
+// animated sun (z-index: 5)
+// layer007 | bldgs-03   | light blue     | 1789x1174
 // layer010 | bldgs-bg   | dark gray      | 2068 × 1456
 // layer050 | clouds-a   | main clouds    | 6437 × 3482
 // layer100 | bldg01     | top and bottom | 410 x 1501
@@ -218,7 +228,7 @@ MovingLayer.prototype.layerSetup = function () {
 //   size of viewport and size of image
 
 l000.layerSetup();
-l001.layerSetup();
+l007.layerSetup();
 l010.layerSetup();
 l050.layerSetup();
 l100.layerSetup();
@@ -227,12 +237,77 @@ l100.layerSetup();
 // main loop: move them
 var t = 0;
 setInterval(function () {
-  "use strict";// l000.layerMove(100*Math.cos(t*Math.PI/180), 100*Math.sin(t*Math.PI/180));
-  // l000.layerMove(m.get_x(), m.get_y());
   l000.layerMove();
-  l001.layerMove();
+  l007.layerMove();
   l010.layerMove();
   l050.layerMove();
   l100.layerMove();
   ++t;
 }, 1000 / 30);
+
+
+// animated sun jellyfish
+(function ($) {
+  "use strict";
+	$.fn.animateimage = function (framerate, repeats) {
+    return this.each(function () {
+      var
+				$this = $(this), duration, spritesize, image, horizontal, spritecount,
+				i, hidden, visible, lastimage;
+
+      framerate = Math.abs(framerate || 10);
+      if (typeof repeats === 'undefined' || repeats < -1) { repeats = -1; }
+			duration = 1 / framerate;
+			spritesize = $this.data('spritesize');
+      image = $this.children(); // grab all children (images) of the target element
+
+      if (spritesize) { // sprite sheet
+        if (image.length === 1) { // image should be a single image containing the sprite sheet
+          horizontal = ($this.data('spritedirection') !== 'vertical');
+          spritecount = (horizontal ? image.width() : image.height()) / spritesize;
+          TweenLite.set(image, { visibility: 'visible' });
+
+          // attach a reference to the animation on the element, so it can be easily grabbed outside of the plugin and paused, reversed etc
+          this.animation = new TimelineMax({ repeat: repeats });
+
+          if (horizontal) {
+            TweenLite.set(this, { width: spritesize });
+            for (i = 0; i < spritecount; i++) {
+              this.animation.set(image, { left: "-" + (i * spritesize) + "px" }, i * duration);
+            }
+          } else {
+            TweenLite.set(this, { height: spritesize });
+            for (i = 0; i < spritecount; i++) {
+              this.animation.set(image, { top: "-" + (i * spritesize) + "px" }, i * duration);
+            }
+          }
+          // add an 'empty' set after the last position change - this adds padding at the end of the timeline so the last frame is displayed for the correct duration before the repeat
+          this.animation.set({}, {}, i * duration);
+        }
+
+      } else { // image sequence
+        if (image.length > 1) { // image should only contain the images to be animated
+
+          hidden = { position: 'absolute', visibility: 'hidden' }; // styles for image in the sequcnce
+          visible = { position: 'static', visibility: 'visible' };
+
+          // in case the poster is not the first child, make sure its pre-animated state is disabled
+          TweenLite.set(image.filter('.poster'), hidden);
+
+          lastimage = image.last();
+
+          // attach a reference to the animation on the element, so it can be easily grabbed outside of the plugin and paused, reversed etc
+          this.animation = new TimelineMax({ repeat: repeats })
+              // this first set is not strictly needed as lastimage is underneath all of the other images, but it certainly doesn't hurt
+              .set(lastimage, hidden)
+              // toggle images one by one between visible and hidden - at any one time, only one image will be visible, and its static positioning will set the size for the container
+              .staggerTo(image, 0, visible, duration, 0)
+              // hide all the elements except lastimage - it will be hidden on repeat if needed at the same time as first is shown
+              .staggerTo(image.not(lastimage), 0, $.extend(hidden, { immediateRender: false }), duration, duration)
+              // add an 'empty' set after lastimage is made visible - this adds padding at the end of the timeline so lastimage is displayed for the correct duration before the repeat
+              .set({}, {}, "+=" + duration);
+        }
+      }
+    });
+  };
+}(jQuery));
